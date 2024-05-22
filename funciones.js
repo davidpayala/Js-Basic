9b1dc68eebc1398c887fa96203f27e1eb75b70c4
davidpayala <davidpayala@outlook.com>
2024-05-22 17:42:32 -0500

    Como no tenia donde guardar y no quería incorporar un proyecto adicional, estoy colando el boceto de la pagina de producto de peluca

---
+++ funciones.js
@@ -0,0 +1,10 @@
+//declaraticas
+function miFuncion() {
+    return 3;
+}
+
+//de expresion
+
+var miFuncion = function(a,b) {
+    return a + b;
+} 
--- variables.js
+++ variables.js
@@ -1,1 +1,17 @@
 var nombre= "David";
+//declarar
+var edad;
+
+edad
+
+//inicializar
+
+edad = 30;
+edad
+
+var elementos = ["Computador","Celular"];
+
+var persona={
+    nombre: "Diego",
+    edad: 30
+}
